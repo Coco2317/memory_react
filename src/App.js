990eff140c506c63a1337c17bc070b140467194b
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-11-05 11:41:30 +0100

    button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 
+
+
+
 function App() {
   return (
-    <div>
+    <div classname="App">
+
+
+
+
     </div>
   );
 }
---
+++ src/components/button/button.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Button(props) {
+    return (
+        <button>{props.text}</button>
+            
+         
+    );
+}
+
+export default Button;
