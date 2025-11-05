c9619afef6cbd62190cc871f32349c00aa3b33d0
nicole.castillo <nicole.nicole-castillo@laplateforme.io>
2025-11-05 11:43:18 +0100

    button click me

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 
 
-
+import button from './components/button/button';
 
 function App() {
   return (
     <div classname="App">
 
-
-
+<button  text="Click Me!" />
 
     </div>
   );
